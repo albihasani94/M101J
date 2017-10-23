725379a4d5d995a84284cce9acd536af60a9e94d
ahasani <albihasani94@gmail.com>
2017-10-23 23:01:35 +0200

    add Freemarker templating engine

---
+++ src/main/java/com/mongodb/HelloFreemarker.java
@@ -0,0 +1,29 @@
+package com.mongodb;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HelloFreemarker {
+
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration();
+        configuration.setClassForTemplateLoading(HelloFreemarker.class, "/");
+
+        try {
+            Template template = configuration.getTemplate("hello.ftl");
+            StringWriter writer = new StringWriter();
+            Map<String, Object> helloMap = new HashMap<>();
+            helloMap.put("name", "Freemarker");
+
+            template.process(helloMap, writer);
+            System.out.println(writer);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
