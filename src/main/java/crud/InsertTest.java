b1061da3fa2ea897db3e662c95bd271ff34aa651
ahasani <albihasani94@gmail.com>
2017-11-01 22:07:02 +0100

    crud: insert

---
+++ src/main/java/crud/InsertTest.java
@@ -0,0 +1,22 @@
+package crud;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+public class InsertTest {
+    public static void main(String[] args) {
+        MongoClient client = new MongoClient();
+        MongoDatabase db = client.getDatabase("course");
+        MongoCollection<Document> coll = db.getCollection("insertTest");
+
+        coll.drop();
+
+        Document aqif = new Document("name", "Aqif")
+                .append("age", 30)
+                .append("profession", "Software Develoepr");
+
+        coll.insertOne(aqif);
+    }
+}
