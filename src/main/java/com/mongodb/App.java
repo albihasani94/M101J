f55bf24c58d2faeb9dcfbe9e37c0a8724e0a904d
ahasani <albihasani94@gmail.com>
2017-11-01 21:29:22 +0100

    java driver

--- src/main/java/com/mongodb/App.java
+++ src/main/java/com/mongodb/App.java
@@ -1,13 +1,23 @@
 package com.mongodb;
 
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class  App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        MongoClientOptions options = MongoClientOptions.builder().connectionsPerHost(100).build();
+        MongoClient client = new MongoClient(new ServerAddress(), options);
+
+        MongoDatabase db = client.getDatabase("test").withReadPreference(ReadPreference.secondary());
+
+        MongoCollection<Document> coll = db.getCollection("test");
+
     }
 }
