02329355ea433aeba35a2562cbf0c9ab155fd0c6
ahasani <albihasani94@gmail.com>
2017-11-03 23:58:02 +0100

    add find with filter

--- src/main/java/com/mongodb/crud/FindWithFilterTest.java
+++ src/main/java/com/mongodb/crud/FindWithFilterTest.java
@@ -4,21 +4,22 @@ import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import static com.mongodb.util.Helpers.jsonToString;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.mongodb.util.Helpers.jsonToString;
-
-public class FindTest {
+public class FindWithFilterTest {
     public static void main(String[] args) {
         MongoClient client = new MongoClient();
         MongoDatabase database = client.getDatabase("course");
         MongoCollection<Document> collection = database.getCollection("insertTest");
 
-       // System.out.println(collection.find().first());
+        Bson filter = new Document("age", new Document("$gt", 25));
 
-        List<Document> all = collection.find().into(new ArrayList<>());
+        List<Document> all = collection.find(filter).into(new ArrayList<>());
         all.forEach(jsonToString());
     }
 }
