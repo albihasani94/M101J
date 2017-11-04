6a7e737ce12554163a190743db20120c834c1396
ahasani <albihasani94@gmail.com>
2017-11-04 19:41:20 +0100

    add mongo to spark/freemarker

--- src/main/java/com/mongodb/HelloSparkFreemarker.java
+++ src/main/java/com/mongodb/HelloSparkFreemarker.java
@@ -1,7 +1,10 @@
 package com.mongodb;
 
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
+import org.bson.Document;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -11,6 +14,8 @@ import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.mongodb.util.Helpers.getDocumentMongoCollection;
+
 public class HelloSparkFreemarker {
 
     public static void main(String[] args) {
@@ -18,16 +23,22 @@ public class HelloSparkFreemarker {
         Configuration configuration = new Configuration();
         configuration.setClassForTemplateLoading(HelloFreemarker.class, "/");
 
+        MongoClient client = new MongoClient();
+        MongoDatabase database = client.getDatabase("course");
+        database.createCollection("sparkFreeMarker");
+        MongoCollection<Document> sparkFreemarker = database.getCollection("sparkFreemarker");
+
+        sparkFreemarker.drop();
+
+        sparkFreemarker.insertOne(new Document("name", "Test"));
+
         Spark.get(new Route("/") {
             public Object handle(Request request, Response response) {
                 StringWriter writer = new StringWriter();
                 try {
                     Template template = configuration.getTemplate("hello.ftl");
-
-                    Map<String, Object> helloMap = new HashMap<>();
-                    helloMap.put("name", "Freemarker");
-
-                    template.process(helloMap, writer);
+                    Document document = sparkFreemarker.find().first();
+                    template.process(document, writer);
                     System.out.println(writer);
                 } catch (Exception e) {
                     e.printStackTrace();
