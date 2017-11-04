695f76f1c8ada54f01d4c0ed9e174a46c135836b
ahasani <albihasani94@gmail.com>
2017-11-04 17:28:51 +0100

    add find with sort/skip/limit

---
+++ src/main/java/com/mongodb/crud/FindWithSortSkipLimitTest.java
@@ -0,0 +1,28 @@
+package com.mongodb.crud;
+
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.mongodb.client.model.Sorts.ascending;
+import static com.mongodb.util.Helpers.getDocumentMongoCollection;
+import static com.mongodb.util.Helpers.jsonToString;
+
+public class FindWithSortSkipLimitTest {
+    public static void main(String[] args) {
+        MongoCollection<Document> collection = getDocumentMongoCollection();
+
+        Bson sort = ascending("age");
+
+        List<Document> all = collection.find()
+                .sort(sort)
+                .skip(1)
+                .limit(1)
+                .into(new ArrayList<>());
+
+        all.forEach(jsonToString());
+    }
+}
---
+++ src/main/java/com/mongodb/crud/RemoveTest.java
@@ -0,0 +1,4 @@
+package com.mongodb.crud;
+
+public class RemoveTest {
+}
