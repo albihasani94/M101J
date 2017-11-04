84bbda08bd692cb4071d4f8aeef83377fb2830f8
ahasani <albihasani94@gmail.com>
2017-11-04 16:18:37 +0100

    new example with projection

--- src/main/java/com/mongodb/crud/FindWithProjectionTest.java
+++ src/main/java/com/mongodb/crud/FindWithProjectionTest.java
@@ -1,12 +1,15 @@
 package com.mongodb.crud;
 
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Projections;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.mongodb.client.model.Projections.exclude;
+import static com.mongodb.client.model.Projections.include;
 import static com.mongodb.util.Helpers.getDocumentMongoCollection;
 import static com.mongodb.util.Helpers.jsonToString;
 
@@ -14,7 +17,8 @@ public class FindWithProjectionTest {
     public static void main(String[] args) {
         MongoCollection<Document> collection = getDocumentMongoCollection();
 
-        Bson projection = new Document("age", 1).append("name", 1);
+        Bson projection = Projections.fields(include("name", "profession"),
+                exclude("_id"));
 
         List<Document> all = collection.find().projection(projection).into(new ArrayList<>());
 
