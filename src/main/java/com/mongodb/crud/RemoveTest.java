ba751a621fcde8f02090c4fb3a9e68ea86eb32b6
ahasani <albihasani94@gmail.com>
2017-11-04 18:50:58 +0100

    add remove operation

--- src/main/java/com/mongodb/crud/RemoveTest.java
+++ src/main/java/com/mongodb/crud/RemoveTest.java
@@ -1,4 +1,17 @@
 package com.mongodb.crud;
 
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.util.Helpers.getDocumentMongoCollection;
+
 public class RemoveTest {
+    public static void main(String[] args) {
+        MongoCollection<Document> collection = getDocumentMongoCollection();
+
+        collection.deleteOne(eq("_id", new ObjectId("59fde79595b4ff2fa0c44e95")));
+    }
 }
